4053ebd74124c72f2e0d9fd9236f19e44e0c9116
Tomoaki Takaichi <totakaic@yahoo-corp.jp>
2020-09-25 11:41:09 +0900

    init commit

---
+++ src/main/java/com/takaichi00/selenidecucumbersample/SelenideCucumberSampleApplication.java
@@ -0,0 +1,13 @@
+package com.takaichi00.selenidecucumbersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelenideCucumberSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelenideCucumberSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takaichi00/selenidecucumbersample/SelenideCucumberSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takaichi00.selenidecucumbersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelenideCucumberSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
