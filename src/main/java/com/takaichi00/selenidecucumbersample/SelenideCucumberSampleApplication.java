65aa454c8343fee068e6e2a2fdeab86ee6af7c83
TomoakiTakaichi <kiruhihhohu555@gmail.com>
2020-09-27 00:29:22 +0900

    feat: create application form page

--- src/main/java/com/takaichi00/selenidecucumbersample/SelenideCucumberSampleApplication.java
+++ src/main/java/com/takaichi00/selenidecucumbersample/SelenideCucumberSampleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SelenideCucumberSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SelenideCucumberSampleApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SelenideCucumberSampleApplication.class, args);
+  }
 }
---
+++ src/main/java/com/takaichi00/selenidecucumbersample/application/ApplicationFormController.java
@@ -0,0 +1,17 @@
+package com.takaichi00.selenidecucumbersample.application;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ApplicationFormController {
+
+  @RequestMapping(path = "/createForm", method = {GET, POST})
+  public String showBootPage() {
+    return "createForm";
+  }
+
+}
