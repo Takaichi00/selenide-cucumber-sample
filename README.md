3981418507205f5b195c175b8a90a91ffc3c1a54
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-04 19:49:40 +0900

    feat: tmp impl cucumber sample test

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # selenide-cucumber-sample
+
+# Command List
+
+## execute Junit5 Unit Test
+```
+mvn test
+```
+
+## execute Cucumber Test
+```
+mvn failsafe:integration-test
+```
---
+++ src/test/java/com/takaichi00/selenidecucumbersample/cucumber/steps/SampleSteps.java
@@ -0,0 +1,18 @@
+package com.takaichi00.selenidecucumbersample.cucumber.steps;
+
+import io.cucumber.java.ja.ならば;
+import io.cucumber.java.ja.もし;
+
+public class SampleSteps {
+
+  @もし("サンプルページにアクセスした")
+  public void もしサンプルページにアクセスした() {
+
+  }
+
+  @ならば("{string}が表示される")
+  public void ならば_string_が表示される(String sampleStr) {
+
+  }
+
+}
