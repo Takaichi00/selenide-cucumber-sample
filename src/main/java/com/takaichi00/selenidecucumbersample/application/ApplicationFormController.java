0dced1f2b0f82464be1d270c173f328d52d0fb5e
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-07 19:45:28 +0900

    chore: tmp impl Spring Jpa  with H2 database

--- src/main/java/com/takaichi00/selenidecucumbersample/application/ApplicationFormController.java
+++ src/main/java/com/takaichi00/selenidecucumbersample/application/ApplicationFormController.java
@@ -3,14 +3,24 @@ package com.takaichi00.selenidecucumbersample.application;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import com.takaichi00.selenidecucumbersample.integration.SampleEntity;
+import com.takaichi00.selenidecucumbersample.integration.SampleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class ApplicationFormController {
+
+  @Autowired
+  SampleRepository sampleRepository;
 
   @RequestMapping(path = "/sample", method = {GET, POST})
   public String showBootPage() {
+    sampleRepository.save(new SampleEntity(1));
+    log.info(sampleRepository.findAll().toString());
     return "sample";
   }
 
