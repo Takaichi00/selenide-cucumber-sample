17ed08af133abed00fd22adb74921b3ec558a8f3
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-09 18:37:48 +0900

    refactor: remove unused component

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ CREATE TABLE IF NOT EXISTS sample (
 );
 
 INSERT INTO sample (id) VALUES(1);
+
+INSERT INTO books (id, title) VALUES(1, 'title1');
+INSERT INTO books (id, title) VALUES(2, 'title2');
