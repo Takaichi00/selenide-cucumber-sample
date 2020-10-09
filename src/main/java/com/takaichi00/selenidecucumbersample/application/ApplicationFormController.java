adc2f1d77ceaddc9773791b846375d3523172f72
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2020-10-09 17:46:59 +0900

    tmp: imple controller but fail cucumber test

--- src/main/java/com/takaichi00/selenidecucumbersample/application/ApplicationFormController.java
+++ src/main/java/com/takaichi00/selenidecucumbersample/application/ApplicationFormController.java
@@ -3,6 +3,7 @@ package com.takaichi00.selenidecucumbersample.application;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import com.takaichi00.selenidecucumbersample.integration.BookEntity;
 import com.takaichi00.selenidecucumbersample.integration.BookRepository;
 import com.takaichi00.selenidecucumbersample.integration.SampleRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -25,17 +28,23 @@ public class ApplicationFormController {
 
   @RequestMapping(path = "/sample", method = {GET})
   public String showSamplePage() {
-//    log.info(sampleRepository.findAll().toString());
     return "sample";
   }
 
   @RequestMapping(path = "/top", method = {GET})
   public String showTopPage(Model model) {
-//    log.info(sampleRepository.findAll().toString());
 
-    BookForDisplay book = BookForDisplay.builder().title("title1").build();
+    List<BookEntity> bookEntities = bookRepository.findAll();
 
-    model.addAttribute("bookList", Arrays.asList(book));
+    List<BookForDisplay> bookForDisplays = new ArrayList<>();
+
+    for (BookEntity bookEntity : bookEntities) {
+      bookForDisplays.add(BookForDisplay.builder()
+                                        .title(bookEntity.getTitle())
+                                        .build());
+    }
+
+    model.addAttribute("bookList", bookForDisplays);
     return "top";
   }
 
